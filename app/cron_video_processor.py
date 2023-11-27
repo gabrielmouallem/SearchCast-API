a23b8e7a47c29cd74a05a4277848829b33587274
gabrielmouallem <gabriel.unifei2017@gmail.com>
2023-11-27 15:29:48 -0300

    feat: more implementations

--- app/cron_video_processor.py
+++ app/cron_video_processor.py
@@ -36,6 +36,9 @@ playlist_urls = [
 
 # def start(url: str):
 def start():
+    confirmation = input("Are you sure (yes/no) ? Remember the billing costs! ")
+    if str(confirmation) == "yes":
+        print("Okay, here we go...")
     VideoProcessingService().process_by_playlist_urls(
         playlist_urls=playlist_urls, max_videos_per_channel=10
     )
