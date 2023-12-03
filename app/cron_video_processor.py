73d7ac46df040427294137d48a91e6a16497729a
gabrielmouallem <gabriel.unifei2017@gmail.com>
2023-12-03 01:15:13 -0300

    feat: added relevant podcasts to the list

--- app/cron_video_processor.py
+++ app/cron_video_processor.py
@@ -6,13 +6,20 @@ setup()
 
 mongo_uri = os.environ.get("MONGO_URI")
 
+# https://www.youtube.com/podcasts/popularshows
+
 playlist_urls = [
-    "https://www.youtube.com/watch?v=RGRPikhvsfw&list=PLWieWKZeFoVQBl5jPTEvWUah6-FTufaKi",
-    "https://www.youtube.com/watch?v=Po-ja6buGy4&list=PLWieWKZeFoVRmUNn9KA2dVpQswo19QkwQ",
     "https://www.youtube.com/watch?v=deKKy5uzPaU&list=PLJznpI7w9TooV0ORuL9e4ZRYMznV_Dwdp",
+    "https://www.youtube.com/watch?v=6gqjTv8AzOw&list=PLV-b4RRsU-VYLTSoqXz9J2abV2-MWh6eq",
+    "https://www.youtube.com/watch?v=deKKy5uzPaU&list=PLJznpI7w9TooV0ORuL9e4ZRYMznV_Dwdp",
+    "https://www.youtube.com/watch?v=Po-ja6buGy4&list=PLWieWKZeFoVRmUNn9KA2dVpQswo19QkwQ",
     "https://www.youtube.com/watch?v=PBWYMa3nuiw&list=PLaE_mZALZ0V2E0lVJowee_oerd3OMvyJu",
-    "https://www.youtube.com/watch?v=n-53I1tfnIw&list=PLczDDIRnclWRAi9rLUxxUHz2j7XSEothf",
+    "https://www.youtube.com/watch?v=u3jjwO0egAM&list=PLk_e6laOs_lSC7O_qcR9vgBg_EVvVSF33",
     "https://www.youtube.com/watch?v=8Bkl7KEbw2Q&list=PLByCI4BxQvkSJKS_sj01Vg_5kuyBO3bsq",
+    "https://www.youtube.com/watch?v=WqiLEK-Lzw4&list=PLWBcDIDlsDmq6o7Qvyn_NfHcKpLYgrjI8",
+    "https://www.youtube.com/watch?v=ObdvuVmYOy4&list=PLkKsdR0a6X5Qz26LZ20WcukTWbNUVXAVN",
+    "https://www.youtube.com/watch?v=9JyRGOiBt0o&list=PL2LH9u21t_RQC1viFOKJOvUa9aPwi9Aez",
+    "https://www.youtube.com/watch?v=n-53I1tfnIw&list=PLczDDIRnclWRAi9rLUxxUHz2j7XSEothf",
 ]
 
 
