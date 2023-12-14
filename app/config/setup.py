51b29bd26e09b0fe2de8bdfb9e951933d3766d1d
gabrielmouallem <gabriel.unifei2017@gmail.com>
2023-12-14 20:43:09 -0300

    fix: dotenv vars not loading after .env file update

--- app/config/setup.py
+++ app/config/setup.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 
 def setup():
-    load_dotenv(dotenv_path=".env", verbose=True)
+    load_dotenv(dotenv_path=".env", verbose=True, override=True)
 
     env = os.environ.get("FLASK_ENV", "development")
     print(f"\n * API env: {env.upper()}\n")
     if env != "production":
         dotenv_path = f".env.{env}"
-        load_dotenv(dotenv_path=dotenv_path, verbose=True)
+        load_dotenv(dotenv_path=dotenv_path, verbose=True, override=True)
     return env
