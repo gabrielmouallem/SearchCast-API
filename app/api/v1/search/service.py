b25a62f203f4206814aaef8d37961a3b6cee885d
gabrielmouallem <gabriel.unifei2017@gmail.com>
2023-12-14 22:58:24 -0300

    refactor: removed unused code

--- app/api/v1/search/service.py
+++ app/api/v1/search/service.py
@@ -2,10 +2,6 @@
 from .repository import SearchRepository
 
 
-def get_publish_date(item):
-    return item["video"]["publishDate"]
-
-
 class SearchService:
     def search_transcriptions(self, query, page, per_page):
         return SearchRepository().search_transcriptions(
