ebe3ec3747150008c306fd1ecf9dcb6c6010ee95
gabrielmouallem <gabriel.unifei2017@gmail.com>
2023-12-18 20:16:55 -0300

    feat: code cleaning on existing endpoints and added '/search_by_video' endpoint

--- app/api/v1/search/controller.py
+++ app/api/v1/search/controller.py
@@ -1,5 +1,6 @@
 # controller.py
 from flask import jsonify, Response
+import pymongo
 from .dto import SearchDTO
 from .service import SearchService
 
@@ -17,34 +18,91 @@ class SearchController:
                 "$text": {"$search": format_text_to_double_quotes(search.query_text)}
             }
 
-            # Modify the text search query based on case sensitivity
-            if not search.case_sensitive:
-                text_search_query["$text"]["$caseSensitive"] = False
-
-            # If exactText is True, use an exact match query
-            if search.exact_text:
-                exact_text_query = {"text": search.query_text}
-            else:
-                exact_text_query = {}
-
-            # Combine the text search query and the exact text query
-            combined_query = {"$and": [text_search_query, exact_text_query]}
-
             # Perform the search with pagination
             result_data = self.search_service.search_transcriptions(
-                query=combined_query,
+                query=text_search_query,
                 page=search.page,
                 per_page=search.per_page,
             )
 
             result_count = self.search_service.count_transcriptions(
-                query=combined_query
+                query=text_search_query
             )
 
             response_data = {
                 "page": search.page,
                 "results": result_data,
                 "count": result_count,
+            }
+
+            return jsonify(response_data)
+
+        except Exception as e:
+            return Response(
+                response=str(e),
+                status=500,
+                mimetype="application/json",
+            )
+
+    def search_transcriptions_by_video(self, search: SearchDTO):
+        try:
+            skip_count = (search.page - 1) * search.per_page
+
+            common_pipeline = [
+                {
+                    "$match": {
+                        "$text": {
+                            "$search": format_text_to_double_quotes(search.query_text)
+                        }
+                    }
+                },
+                {"$sort": {"video.publishDate": pymongo.DESCENDING}},
+                {
+                    "$group": {
+                        "_id": "$video._id",
+                        "transcriptions": {
+                            "$push": {
+                                "text": "$text",
+                                "start": "$start",
+                                "duration": "$duration",
+                            }
+                        },
+                        "video": {"$first": "$video"},
+                    }
+                },
+            ]
+            # Create the text search query
+            results_pipeline = common_pipeline + [
+                {"$skip": skip_count},
+                {"$limit": search.per_page},
+                {
+                    "$group": {
+                        "_id": None,
+                        "results": {"$push": "$$ROOT"},
+                    }
+                },
+                {"$project": {"_id": 0, "results": 1, "count": 1}},
+            ]
+
+            count_pipeline = common_pipeline + [
+                {
+                    "$count": "count",
+                },
+            ]
+
+            # Perform the search with pagination
+            result_data = self.search_service.aggregate_transcriptions(
+                pipeline=results_pipeline
+            )
+
+            count_data = self.search_service.aggregate_transcriptions(
+                pipeline=count_pipeline
+            )
+
+            response_data = {
+                **result_data,
+                **count_data,
+                "page": search.page,
             }
 
             return jsonify(response_data)
--- app/api/v1/search/dto.py
+++ app/api/v1/search/dto.py
@@ -1,7 +1,5 @@
 class SearchDTO:
-    def __init__(self, query_text, page, per_page, case_sensitive, exact_text):
+    def __init__(self, query_text, page, per_page):
         self.query_text = query_text
         self.page = page
         self.per_page = per_page
-        self.case_sensitive = case_sensitive
-        self.exact_text = exact_text
