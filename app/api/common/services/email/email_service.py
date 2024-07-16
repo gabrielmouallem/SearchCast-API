880c698e74759a607357fb5129253c076e68df6f
gabrielmouallem <gabriel.unifei2017@gmail.com>
2024-07-16 10:05:00 -0300

    fix: minor adjustments

--- app/api/common/services/email/email_service.py
+++ app/api/common/services/email/email_service.py
@@ -16,7 +16,7 @@ class EmailService:
             return
 
         params: resend.Emails.SendParams = {
-            "from": "Contato <contato@searchcast.app>",
+            "from": "SearchCast <contato@searchcast.app>",
             "to": [receiver_email],
             "subject": subject,
             "html": html_body,
